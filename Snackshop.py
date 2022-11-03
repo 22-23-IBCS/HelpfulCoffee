d9973bff95f4273fb15569f2d9e0bbc6875aacdf
Yuheng Chen <yuheng_chen@m018739.aw.org>
2022-11-03 13:12:26 -0700

    added snackshop

---
+++ Snackshop.py
@@ -0,0 +1,37 @@
+def main():
+
+    drink = {"Boba" : 6.69,
+             "Mojito" : 10.00,
+             "Orange Juice" : 3.5,
+             "Coke" : 2.5,
+             "Cappuccino" : 4.89}
+
+    print(list(drink.keys()))
+    request = input("Which drink would you like to buy?\n")
+    price = drink.get(request)
+    many = int(input("How many would you want?\n"))
+    print("Here you go!\nYou got " + str(many) + " " + request)
+    print("That would be $" + str(round(price*many, 2)) + " dollars")
+
+
+
+    toBuy = []
+    while True:
+        print(list(drink.keys()))
+        res = input("What would you like to buy? Enter 'stop' if done\n")
+        if res == "stop":
+            break
+        else:
+            request1 = input("Which drink would you like to buy?\n")
+            price1= (list(drink.keys()))
+            many1 = int(input("How many would you want?\n"))
+            price1 = drink.get(request1)
+            print("Here you go!\nYou got " + str(many1) + " " + request1)
+            print("That would be $" + str(round(price1*many1, 2)) + " dollars")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
